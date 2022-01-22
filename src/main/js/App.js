9856950d26d47c5213ac0457fe7cce1fd6d2f277
Joshua Proctor <jproctor@redventures.com>
2022-01-22 09:54:31 -0500

    feat: Added pages folder structure

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/login/login";
+import NewStory from "./pages/new_story/new_story";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/account" element={<Login />}></Route>
+          <Route exact path="/new-story" element={<NewStory />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/main/js/pages/new_story/new_story.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewStory = () => {
+  return (
+    <>
+      <h1>New Story</h1>
+    </>
+  );
+};
+
+export default NewStory;
