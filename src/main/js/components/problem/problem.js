662127b8de607f23bb888705292aa28349c951f3
Artea Wright <artea.wright@truist.com>
2022-03-25 17:05:49 -0400

    Update

--- src/main/js/components/problem/problem.js
+++ src/main/js/components/problem/problem.js
@@ -1,12 +1,23 @@
-import React from "react";
+import { useState} from "react";
 
 const Problem = (props) => {
-    return(
-        <div>
-          <label>
-            <input type="radio" value=""/>
-            {props.problem}
+
+ const [itemChecked, setitemChecked] = useState(false);
+
+ const completedChallenge = (e) => {
+    setitemChecked(!itemChecked);
+    console.log("done");
+  e.setAttribute("disabled", "true");
+};
+
+    return (
+        <div className="popup__challenges">
+          <span className="popup__accordion">+</span>
+          <label htmlFor="popup__challenge__label">
+          {props.problem}
           </label>
+
+          <input className="popup__challenge__checkbox" type="checkbox" checked={itemChecked} onChange={completedChallenge}/>
         </div>
     )
 }
