cdb6eb73092b7ea50f91d80f7556830b187907fa
Joshua Proctor <81370795+joshuaeup-rv@users.noreply.github.com>
2022-04-05 20:58:11 -0400

    feat: Build app (#1)

---
+++ src/main/js/pages/feed/feed.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Navigation from "../../components/navigation/navigation";
+import NewWriter from "../../components/newWriter/newWriter";
+import Post from "../../components/post/post";
+import { getStorageValue } from "../../utils/useLocalStorage";
+
+const Home = (props) => {
+    const [posts, setPosts] = useState(props.posts);
+
+    // Function that triggers as soon as the page loads
+    useEffect(() => {
+        // Scroll to the top of the screen as the page loads
+        window.scrollTo(0, 0);
+
+        // Updates state variable to store the values in local storage at a specified key
+        // And provide a default value of props.posts which is a json file declared in the file structure
+        setPosts(getStorageValue("post", props.posts));
+    }, []);
+
+    return (
+        <>
+            <Navigation title="LOGO " />
+            <div id="home">
+                <div>
+                    {/* Displays the posts stored in the posts state variable */}
+                    {posts.map((post) => {
+                        return (
+                            <Link to={`/story/${post.id}`} key={post.id}>
+                                <Post
+                                    title={post.title}
+                                    postImage={post.postImage}
+                                    text={post.text}
+                                    author={post.author}
+                                    group={post.group}
+                                    date={post.date}
+                                    read_length={post.read_length}
+                                />
+                            </Link>
+                        );
+                    })}
+                </div>
+                {/* Displays the New Writer UI Component */}
+                <NewWriter />
+            </div>
+        </>
+    );
+};
+
+export default Home;
